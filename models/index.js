f3dbd14c0beedc4f165eb7124f5e110a17df4e9b
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-29 20:58:09 -0400

    updating model to avoid creation of wrong table name

--- models/index.js
+++ models/index.js
@@ -17,12 +17,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag'
+  through: 'product_tag'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag'
+  through: 'product_tag'
 });
 
 module.exports = {
