4b323ff61a89e7f52c231714d7d6df03c56bfcac
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-30 08:56:05 -0400

    updated product routes to remove async callout

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,18 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  try {
-    const prodList = await Product.findAll({
+Product.findAll({
       include: [{model: Category},{model: Tag}]
-    });
-    res.status(200).json(prodList);
-  } catch (err) {
+    }).then(data => {res.status(200).json(data)})
+    .catch((err) => {
     res.status(500).json(err);
-  }
-});
+  })
+})
 
 // get one product
 router.get('/:id', (req, res) => {
